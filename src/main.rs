bcf5ded03a716bfde02b1b8b811dba301fc91a7c
Pratham Bhatnagar <pratham@Prathams-MacBook-Air.local>
2023-01-03 01:35:45 +0530

    Basic rust

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,12 @@ struct Rectangle {
 }
 
 impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+}
+
+impl Rectangle {
     fn can_hold(&self, other: &Rectangle) -> bool {
         self.width > other.width && self.height > other.height
     }
